8b52448f3c6b4823da427afe3e1470e05e9ca27e
Onur <onur.suyalcinkaya@gmail.com>
2024-05-09 10:55:37 +0200

    chore(website): add analytics (#14)

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/globals.css'
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
+import Script from 'next/script'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import type { Metadata, Viewport } from 'next'
 
 import { Button } from '@/components/ui/button'
@@ -47,6 +49,13 @@ export default function RootLayout({
         </footer>
         <TailwindIndicator />
       </body>
+      <Script
+        src="https://unpkg.com/@tinybirdco/flock.js"
+        data-host="https://api.tinybird.co"
+        data-token={process.env.NEXT_PUBLIC_TINYBIRD_TOKEN}
+        strategy="lazyOnload"
+      />
+      <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_GA_ID}`} />
     </html>
   )
 }
