75a8df45a7c7b6b50b3e2ab30ebdfaecd02ffe8a
suyalcinkaya <onur.suyalcinkaya@gmail.com>
2024-05-09 13:53:14 +0200

    chore: improve animation section - closes #17

--- apps/website/src/components/hero/sections/animation.tsx
+++ apps/website/src/components/hero/sections/animation.tsx
@@ -1,27 +1,67 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { Gauge } from '@suyalcinkaya/gauge'
+import { useInView } from 'react-intersection-observer'
 
 import { CodeBlock } from '@/components/code-block'
+
 import { Button } from '@/components/ui/button'
 
+const initialGauges = [
+  { value: 18, showAnimation: true, showValue: true },
+  { value: 18, showAnimation: true, showValue: true, variant: 'descending' as const },
+  { value: 81, showAnimation: true, showValue: true },
+  { value: 81, showAnimation: true, showValue: true, variant: 'descending' as const }
+]
+
 export const Animation = () => {
+  const [gauges, setGauges] = useState(initialGauges)
+  const [isAnimationPlaying, setIsAnimationPlaying] = useState(false)
+  const { ref, inView } = useInView({ threshold: 0, triggerOnce: true })
+
+  const playAnimation = () => {
+    if (isAnimationPlaying) return
+
+    setIsAnimationPlaying(true)
+    setGauges(
+      gauges.map((gauge) => ({
+        ...gauge,
+        value: gauge.variant === 'descending' ? 100 : 0,
+        showAnimation: false
+      }))
+    )
+
+    setTimeout(() => {
+      setGauges(initialGauges)
+
+      setTimeout(() => {
+        // Animation transition duration is 1s
+        setIsAnimationPlaying(false)
+      }, 1000)
+    }, 500)
+  }
+
+  // Play animation when the component is in view
+  useEffect(() => {
+    if (inView) {
+      playAnimation()
+    }
+  }, [inView])
+
   return (
     <>
       <h2>Animation</h2>
       <p className="subtitle">
         On initial render, the <code className="inline-code">showAnimation</code> prop animates the Gauge from 0 to the
         value for the <code className="inline-code">ascending</code> variant, and from 100 to the value for the{' '}
-        <code className="inline-code">descending</code> variant.{' '}
-        <Button
-          variant="link"
-          className="inline h-fit whitespace-pre-line p-0 text-base"
-          onClick={() => window.location.reload()}
-        >
-          Refresh the page
-        </Button>{' '}
-        to see the animation.
+        <code className="inline-code">descending</code> variant.
       </p>
+
+      <Button onClick={playAnimation} disabled={isAnimationPlaying} className="mb-8">
+        Replay animation
+      </Button>
+
       <CodeBlock
         code={`import { Gauge } from '@suyalcinkaya/gauge'
 
@@ -36,11 +76,10 @@ export function Component(): JSX.Element {
   )
 }`}
         component={
-          <div className="flex items-center gap-8">
-            <Gauge value={18} showAnimation showValue />
-            <Gauge value={18} showAnimation showValue variant="descending" />
-            <Gauge value={81} showAnimation showValue />
-            <Gauge value={81} showAnimation showValue variant="descending" />
+          <div className="flex items-center gap-8" ref={ref}>
+            {gauges.map((gauge, gaugeIndex) => (
+              <Gauge key={`gauge_${gaugeIndex}`} {...gauge} value={gauge.value} showAnimation={gauge.showAnimation} />
+            ))}
           </div>
         }
       />
--- apps/website/src/components/ui/select.tsx
+++ apps/website/src/components/ui/select.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import { ChevronsUpDownIcon, CheckIcon, ChevronDownIcon, ChevronUpIcon } from 'lucide-react'
+import { LuChevronsUpDown, LuCheck, LuChevronDown, LuChevronUp } from 'react-icons/lu'
 import * as SelectPrimitive from '@radix-ui/react-select'
 
 import { cn } from '@/lib/utils'
@@ -26,7 +26,7 @@ const SelectTrigger = React.forwardRef<
   >
     {children}
     <SelectPrimitive.Icon asChild>
-      <ChevronsUpDownIcon className="h-4 w-4 opacity-50" />
+      <LuChevronsUpDown className="h-4 w-4 opacity-50" />
     </SelectPrimitive.Icon>
   </SelectPrimitive.Trigger>
 ))
@@ -41,7 +41,7 @@ const SelectScrollUpButton = React.forwardRef<
     className={cn('flex cursor-default items-center justify-center py-1', className)}
     {...props}
   >
-    <ChevronUpIcon />
+    <LuChevronUp />
   </SelectPrimitive.ScrollUpButton>
 ))
 SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName
@@ -55,7 +55,7 @@ const SelectScrollDownButton = React.forwardRef<
     className={cn('flex cursor-default items-center justify-center py-1', className)}
     {...props}
   >
-    <ChevronDownIcon />
+    <LuChevronDown />
   </SelectPrimitive.ScrollDownButton>
 ))
 SelectScrollDownButton.displayName = SelectPrimitive.ScrollDownButton.displayName
@@ -114,7 +114,7 @@ const SelectItem = React.forwardRef<
   >
     <span className="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
-        <CheckIcon className="h-4 w-4" />
+        <LuCheck className="h-4 w-4" />
       </SelectPrimitive.ItemIndicator>
     </span>
     <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
